d3a593a367c4e682ef9825b474e214bbd31d8d7f
Alex <Alex@201-1>
2019-12-07 13:44:57 +0200

    DataTypes start

---
+++ DataTypes/DataTypes/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::cout << false << std::endl;
+	std::cout << (bool)-123 << std::endl;
+	bool is_summer = false;
+	bool is_day = true;
+}
